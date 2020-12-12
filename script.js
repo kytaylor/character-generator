b1613fea8b8f53f2b4b6674fb144b47bc99e36dc
kytaylor <ky.taylor5468@gmail.com>
2020-12-11 16:02:22 -0800

    can assign stats to each attribute in order

--- script.js
+++ script.js
@@ -68,122 +68,30 @@ var intButton = document.querySelector(".int-button");
 var wisButton = document.querySelector(".wis-button");
 var chaButton = document.querySelector(".cha-button");
 
-// Button event listeners
-strButton.addEventListener("click", assignStr);
-dexButton.addEventListener("click", assignDex);
-conButton.addEventListener("click", assignCon);
-intButton.addEventListener("click", assignInt);
-wisButton.addEventListener("click", assignWis);
-chaButton.addEventListener("click", assignCha);
-
-// Function for determining stats
-// function statOne() {
-//     // Rolls four six-sided dice
-//     var diceOne = Math.floor(Math.random() * 6) + 1;
-//     console.log(diceOne)
-//     rollOne.textContent = diceOne;
-//     var diceTwo = Math.floor(Math.random() * 6) + 1;
-//     console.log(diceTwo)
-//     rollTwo.textContent = diceTwo;
-//     var diceThree = Math.floor(Math.random() * 6) + 1;
-//     console.log(diceThree)
-//     rollThree.textContent = diceThree;
-//     var diceFour = Math.floor(Math.random() * 6) + 1;
-//     console.log(diceFour)
-//     rollFour.textContent = diceFour;
-
-//     // Adds the three highest dice values to an array
-//     var values = [];
-
-//     if (diceOne <= diceTwo && diceOne <= diceThree && diceOne <= diceFour) {
-//         values = values.concat(diceTwo);
-//         values = values.concat(diceThree);
-//         values = values.concat(diceFour);
-//     } else if (diceTwo <= diceOne && diceTwo <= diceThree && diceTwo <= diceFour) {
-//         values = values.concat(diceOne);
-//         values = values.concat(diceThree);
-//         values = values.concat(diceFour);
-//     } else if (diceThree <= diceOne && diceThree <= diceTwo && diceThree <= diceFour) {
-//         values = values.concat(diceOne);
-//         values = values.concat(diceTwo);
-//         values = values.concat(diceFour);
-//     } else if (diceFour <= diceOne && diceFour <= diceTwo && diceFour <= diceThree) {
-//         values = values.concat(diceOne);
-//         values = values.concat(diceTwo);
-//         values = values.concat(diceThree);
-//     }
-//     console.log(values)
-
-//     // Turns array into seperate variables, adds together
-//     var valOne = values[0];
-//     console.log(valOne)
-//     var valTwo = values[1];
-//     console.log(valTwo)
-//     var valThree = values[2];
-//     console.log(valThree)
-
-//     var finalStatOne = valOne + valTwo + valThree;
-//     console.log(finalStatOne)
-//     firstStat.textContent = finalStatOne;
-
-//     // Determines stat modifier based on final stat
-//     if (finalStatOne === 2 || finalStatOne === 3) {
-//         var modifierOne = "-";
-//         var modifierOneNum = 4;
-//     } else if (finalStatOne === 4 || finalStatOne === 5) {
-//         var modifierOne = "-";
-//         var modifierOneNum = 3;
-//     } else if (finalStatOne === 6 || finalStatOne === 7) {
-//         var modifierOne = "-";
-//         var modifierOneNum = 2;
-//     } else if (finalStatOne === 8 || finalStatOne === 9) {
-//         var modifierOne = "-";
-//         var modifierOneNum = 1;
-//     } else if (finalStatOne === 10 || finalStatOne === 11) {
-//         var modifierOne = "+";
-//         var modifierOneNum = 0;
-//     } else if (finalStatOne === 12 || finalStatOne === 13) {
-//         var modifierOne = "+";
-//         var modifierOneNum = 1;
-//     } else if (finalStatOne === 14 || finalStatOne === 15) {
-//         var modifierOne = "+";
-//         var modifierOneNum = 2;
-//     } else if (finalStatOne === 16 || finalStatOne === 17) {
-//         var modifierOne = "+";
-//         var modifierOneNum = 3;
-//     } else if (finalStatOne === 18 || finalStatOne === 19) {
-//         var modifierOne = "+";
-//         var modifierOneNum = 4;
-//     } else if (finalStatOne === 20) {
-//         var modifierOne = "+";
-//         var modifierOneNum = 5;
-//     }
-//     console.log(modifierOne)
-//     console.log(modifierOneNum)
-
-//     // Turns modifier into one string
-//     var finalMod = modifierOne + modifierOneNum;
-//     console.log(finalMod)
-//     modOne.textContent = finalMod;
-// }
+// // Button event listeners
+// strButton.addEventListener("click", assignStr);
+// dexButton.addEventListener("click", assignDex);
+// conButton.addEventListener("click", assignCon);
+// intButton.addEventListener("click", assignInt);
+// wisButton.addEventListener("click", assignWis);
+// chaButton.addEventListener("click", assignCha);
 
 // ----------------------------------------------
-
 
 // Function that rolls each stat
 function statNumber() {
     // Rolls four six-sided dice
     var diceOne = Math.floor(Math.random() * 6) + 1;
-    console.log(diceOne)
+    //console.log(diceOne)
     rollOne.textContent = diceOne;
     var diceTwo = Math.floor(Math.random() * 6) + 1;
-    console.log(diceTwo)
+    //console.log(diceTwo)
     rollTwo.textContent = diceTwo;
     var diceThree = Math.floor(Math.random() * 6) + 1;
-    console.log(diceThree)
+    //console.log(diceThree)
     rollThree.textContent = diceThree;
     var diceFour = Math.floor(Math.random() * 6) + 1;
-    console.log(diceFour)
+    //console.log(diceFour)
     rollFour.textContent = diceFour;
 
     // Adds the three highest dice values to an array
@@ -206,18 +114,18 @@ function statNumber() {
         values = values.concat(diceTwo);
         values = values.concat(diceThree);
     }
-    console.log(values)
+    //console.log(values)
 
     // Turns array into seperate variables, adds together
     var valOne = values[0];
-    console.log(valOne)
+    //console.log(valOne)
     var valTwo = values[1];
-    console.log(valTwo)
+    //console.log(valTwo)
     var valThree = values[2];
-    console.log(valThree)
+    //console.log(valThree)
 
     var finalStatOne = valOne + valTwo + valThree;
-    console.log(finalStatOne)
+    //console.log(finalStatOne)
     return finalStatOne
 }
 
@@ -254,12 +162,12 @@ function modifier(finalStatOne) {
         var modifierOne = "+";
         var modifierOneNum = 5;
     }
-    console.log(modifierOne)
-    console.log(modifierOneNum)
+    // console.log(modifierOne)
+    // console.log(modifierOneNum)
 
     // Turns modifier into one string
     var finalMod = modifierOne + modifierOneNum;
-    console.log(finalMod)
+    //console.log(finalMod)
     return finalMod;
 }
 
@@ -326,32 +234,239 @@ function statSix() {
     rollButton6.style.display = "none";
 }
 
-// ----------------------------------------------
+// -----------------------------------------------------
+
+var counter = -1;
+console.log(counter)
+
+// ------------------------------------------------------
+
+// Assigns stats based on what button is pressed
+// Stats are assigned one at a time, from stat 1 to stat 6 in order
+strButton.addEventListener("click", strButtonFunction);
+
+function strButtonFunction() {
+    counter++
+
+    if (counter === 0) {
+        var statAssign = firstStat.textContent;
+        var modAssign = modOne.textContent;
+    } else if (counter === 1) {
+        statAssign = secondStat.textContent;
+        modAssign = modTwo.textContent;
+    } else if (counter === 2) {
+        statAssign = thirdStat.textContent;
+        modAssign = modThree.textContent;
+    } else if (counter === 3) {
+        statAssign = fourthStat.textContent;
+        modAssign = modFour.textContent;
+    } else if (counter === 4) {
+        statAssign = fifthStat.textContent;
+        modAssign = modFive.textContent;
+    } else if (counter === 5) {
+        statAssign = sixthStat.textContent;
+        modAssign = modSix.textContent;
+    }
+    console.log(counter)
+    console.log(firstStat.textContent)
+    console.log(modOne.textContent)
+    console.log(statAssign)
+    console.log(modAssign)
+
+    assignStr(statAssign, modAssign)
+
+    strButton.style.display = "none";
+}
+
+dexButton.addEventListener("click", dexButtonFunction);
+
+function dexButtonFunction() {
+    counter++
+
+    if (counter === 0) {
+        var statAssign = firstStat.textContent;
+        var modAssign = modOne.textContent;
+    } else if (counter === 1) {
+        statAssign = secondStat.textContent;
+        modAssign = modTwo.textContent;
+    } else if (counter === 2) {
+        statAssign = thirdStat.textContent;
+        modAssign = modThree.textContent;
+    } else if (counter === 3) {
+        statAssign = fourthStat.textContent;
+        modAssign = modFour.textContent;
+    } else if (counter === 4) {
+        statAssign = fifthStat.textContent;
+        modAssign = modFive.textContent;
+    } else if (counter === 5) {
+        statAssign = sixthStat.textContent;
+        modAssign = modSix.textContent;
+    }
+    console.log(counter)
+    console.log(statAssign)
+    console.log(modAssign)
+
+    assignDex(statAssign, modAssign)
+
+    dexButton.style.display = "none";
+}
+
+conButton.addEventListener("click", conButtonFunction);
+
+function conButtonFunction() {
+    counter++
+
+    if (counter === 0) {
+        var statAssign = firstStat.textContent;
+        var modAssign = modOne.textContent;
+    } else if (counter === 1) {
+        statAssign = secondStat.textContent;
+        modAssign = modTwo.textContent;
+    } else if (counter === 2) {
+        statAssign = thirdStat.textContent;
+        modAssign = modThree.textContent;
+    } else if (counter === 3) {
+        statAssign = fourthStat.textContent;
+        modAssign = modFour.textContent;
+    } else if (counter === 4) {
+        statAssign = fifthStat.textContent;
+        modAssign = modFive.textContent;
+    } else if (counter === 5) {
+        statAssign = sixthStat.textContent;
+        modAssign = modSix.textContent;
+    }
+    console.log(counter)
+    console.log(statAssign)
+    console.log(modAssign)
+
+    assignCon(statAssign, modAssign)
+    
+    conButton.style.display = "none";
+}
+
+intButton.addEventListener("click", intButtonFunction);
+
+function intButtonFunction() {
+    counter++
+
+    if (counter === 0) {
+        var statAssign = firstStat.textContent;
+        var modAssign = modOne.textContent;
+    } else if (counter === 1) {
+        statAssign = secondStat.textContent;
+        modAssign = modTwo.textContent;
+    } else if (counter === 2) {
+        statAssign = thirdStat.textContent;
+        modAssign = modThree.textContent;
+    } else if (counter === 3) {
+        statAssign = fourthStat.textContent;
+        modAssign = modFour.textContent;
+    } else if (counter === 4) {
+        statAssign = fifthStat.textContent;
+        modAssign = modFive.textContent;
+    } else if (counter === 5) {
+        statAssign = sixthStat.textContent;
+        modAssign = modSix.textContent;
+    }
+    console.log(counter)
+    console.log(statAssign)
+    console.log(modAssign)
+
+    assignInt(statAssign, modAssign)
+    
+    intButton.style.display = "none";
+}
+
+wisButton.addEventListener("click", wisButtonFunction);
+
+function wisButtonFunction() {
+    counter++
+
+    if (counter === 0) {
+        var statAssign = firstStat.textContent;
+        var modAssign = modOne.textContent;
+    } else if (counter === 1) {
+        statAssign = secondStat.textContent;
+        modAssign = modTwo.textContent;
+    } else if (counter === 2) {
+        statAssign = thirdStat.textContent;
+        modAssign = modThree.textContent;
+    } else if (counter === 3) {
+        statAssign = fourthStat.textContent;
+        modAssign = modFour.textContent;
+    } else if (counter === 4) {
+        statAssign = fifthStat.textContent;
+        modAssign = modFive.textContent;
+    } else if (counter === 5) {
+        statAssign = sixthStat.textContent;
+        modAssign = modSix.textContent;
+    }
+    console.log(counter)
+    console.log(statAssign)
+    console.log(modAssign)
+
+    assignWis(statAssign, modAssign)
+    
+    wisButton.style.display = "none";
+}
+
+chaButton.addEventListener("click", chaButtonFunction);
+
+function chaButtonFunction() {
+    counter++
+
+    if (counter === 0) {
+        var statAssign = firstStat.textContent;
+        var modAssign = modOne.textContent;
+    } else if (counter === 1) {
+        statAssign = secondStat.textContent;
+        modAssign = modTwo.textContent;
+    } else if (counter === 2) {
+        statAssign = thirdStat.textContent;
+        modAssign = modThree.textContent;
+    } else if (counter === 3) {
+        statAssign = fourthStat.textContent;
+        modAssign = modFour.textContent;
+    } else if (counter === 4) {
+        statAssign = fifthStat.textContent;
+        modAssign = modFive.textContent;
+    } else if (counter === 5) {
+        statAssign = sixthStat.textContent;
+        modAssign = modSix.textContent;
+    }
+    console.log(counter)
+    console.log(statAssign)
+    console.log(modAssign)
+
+    assignCha(statAssign, modAssign)
+    
+    chaButton.style.display = "none";
+}
+
+// ------------------------------------------------------
 
 // Functions for assigning rolled stats to stats
-function assignStr() {
-    strStat.textContent = firstStat.textContent;
-    strMod.textContent = modOne.textContent;
+function assignStr(x, y) {
+    strStat.textContent = x;
+    strMod.textContent = y;
 }
-function assignDex() {
-    dexStat.textContent = firstStat.textContent;
-    dexMod.textContent = modOne.textContent;
+function assignDex(x, y) {
+    dexStat.textContent = x;
+    dexMod.textContent = y;
 }
-function assignCon() {
-    conStat.textContent = firstStat.textContent;
-    conMod.textContent = modOne.textContent;
+function assignCon(x, y) {
+    conStat.textContent = x;
+    conMod.textContent = y;
 }
-function assignInt() {
-    intStat.textContent = firstStat.textContent;
-    intMod.textContent = modOne.textContent;
+function assignInt(x, y) {
+    intStat.textContent = x;
+    intMod.textContent = y;
 }
-function assignWis() {
-    wisStat.textContent = firstStat.textContent;
-    wisMod.textContent = modOne.textContent;
+function assignWis(x, y) {
+    wisStat.textContent = x;
+    wisMod.textContent = y;
 }
-function assignCha() {
-    chaStat.textContent = firstStat.textContent;
-    chaMod.textContent = modOne.textContent;
+function assignCha(x, y) {
+    chaStat.textContent = x;
+    chaMod.textContent = y;
 }
-
-// Next: can only assign each rolled stat to one final stat, cannot assign a stat over another, goes through picking stat locations one at a time
